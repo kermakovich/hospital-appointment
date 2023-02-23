ee1b253a30d82beea7e4295079e9bdee4924f384
kermakovich <kermakovich.laba@solvd.com>
2023-02-23 22:46:24 +0300

    Config map was added, secrets were fixed

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cd src/infra
 
 kubectl apply -f pg-secrets.yml
+kubectl apply -f app-configmap.yml
 kubectl apply -f pg-service.yml
 kubectl apply -f app-service.yml
 kubectl apply -f pg-stateful.yml
